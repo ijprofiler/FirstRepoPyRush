36a313b31860af1a62b22e51f899ca471b44475b
ijprofiler <ijprofiler@gmail.com>
2026-01-04 01:43:49 +0300

    platform view

--- init.py
+++ init.py
@@ -1,1 +1,11 @@
-print("Hello repo")
+import platform
+
+print("Operating System:", platform.system())
+print("Node Name:", platform.node())
+print("OS Release:", platform.release())
+print("OS Version:", platform.version())
+print("Machine:", platform.machine())
+print("Processor:", platform.processor())
+print("Architecture:", platform.architecture())
+print("Python Version:", platform.python_version())
+print("Python Compiler:", platform.python_compiler())
